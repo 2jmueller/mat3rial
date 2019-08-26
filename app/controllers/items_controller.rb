ff84494262db3e8ed66300bfeefa4b42d92d5ae7
karim ussene <karimussene@gmail.com>
2019-08-26 16:59:44 +0100

    user and item controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,41 @@ class ItemsController < ApplicationController
 
   def show
     # show a specific item on click
-    @yallah
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    # creates new instance with the params, saves it, and redirects to
+    # user profile page
+    @item = Item.new(item_params)
+    @item.save
+    # redirect to user items list.
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    # finds the item to update by id and updates it with the necessary params
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+  end
+
+  def destroy
+    # finds by id and deletes item
+    @item = Item.find(params[:id])
+    @item.destroy
+    # redirect to user profile page
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :category, :price, :location)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < ApplicationController
+  def index
+    # list all the items in the database
+    @users = User.all
+  end
+
+  def show
+    # show a specific User on click
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # creates new instance with the params, saves it, and redirects to
+    # user profile page
+    @user = User.new(user_params)
+    @user.save
+    # redirect to user items list.
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    # finds the User to update by id and updates it with the necessary params
+    @user = User.find(params[:id])
+    @user.update(item_params)
+  end
+
+  def destroy
+    # finds by id and deletes User
+    @user = User.find(params[:id])
+    @user.destroy
+    # redirect to user profile page
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :user_location, :email)
+  end
 end
