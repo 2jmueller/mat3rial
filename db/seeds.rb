d2ced7272785b5d1313d3c4ca61c628de404320c
karim ussene <karimussene@gmail.com>
2019-08-27 13:01:26 +0100

    user table random seeding, 10 seeds, done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning user table...'
+User.destroy_all
+
+puts 'Creating seeds users...'
+
+users = []
+
+10.times {
+
+  f_name = ['Joao', 'Antonio', 'Julius', 'Rita', 'Ijoline', 'Karim'].sample
+  l_name = ['Muller', 'Laia', 'Ussene', 'Homphomsiltham'].sample
+
+  users << {
+    first_name: f_name,
+    last_name: l_name,
+    user_location:      ['lisboa', 'rio de janeiro', 'luanda', 'estoril', 'benfica'].sample,
+    password:           "111111",
+    email:              "#{f_name + l_name}@mat3rial_unicorn.com"
+
+  }
+}
+User.create!(users)
+puts 'Finished!'
