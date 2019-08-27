c308476c00c8fada1ab9dfa2bbfc85c9c8ec06c2
karim ussene <karimussene@gmail.com>
2019-08-27 13:33:30 +0100

    user table random seeding, 10 seeds + Aria Stark, done

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,19 +13,31 @@ puts 'Creating seeds users...'
 
 users = []
 
+users << {
+    first_name: 'Aria',
+    last_name: 'Star',
+    user_location: 'Winterfel',
+    password: '123456',
+    email: 'ariastark@mat3rial_unicorn.com'
+  }
+
+puts "Creating user Aria Stark"
+
 10.times {
 
   f_name = ['Joao', 'Antonio', 'Julius', 'Rita', 'Ijoline', 'Karim'].sample
-  l_name = ['Muller', 'Laia', 'Ussene', 'Homphomsiltham'].sample
+  l_name = ['Muller', 'Laia', 'Ussene', 'Homphomsiltham', 'Oliveira', 'Fernandes', 'Sousa'].sample
+
+  puts "Creating user #{f_name +" "+ l_name}"
 
   users << {
     first_name: f_name,
     last_name: l_name,
     user_location:      ['lisboa', 'rio de janeiro', 'luanda', 'estoril', 'benfica'].sample,
-    password:           "111111",
+    password:           "123456",
     email:              "#{f_name + l_name}@mat3rial_unicorn.com"
-
   }
+  puts "user created"
 }
 User.create!(users)
 puts 'Finished!'
