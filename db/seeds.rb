772860d9bb0b1fe84e1de9e1e04ba9b2144711e0
karim ussene <karimussene@gmail.com>
2019-08-27 14:25:12 +0100

    changed the user locatioon to universities, added a preventive explanation for errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)jabwf
+
+
+# creates an Aria Stark user plus 10 random users
+# if a seeding fails it is due to repetition in the email (having mutiple exact same names)
 
 puts 'Cleaning user table...'
 User.destroy_all
@@ -28,16 +27,17 @@ puts "Creating user Aria Stark"
   f_name = ['Joao', 'Antonio', 'Julius', 'Rita', 'Ijoline', 'Karim'].sample
   l_name = ['Muller', 'Laia', 'Ussene', 'Homphomsiltham', 'Oliveira', 'Fernandes', 'Sousa'].sample
 
-  puts "Creating user #{f_name +" "+ l_name}"
+  puts "** Generating user #{f_name +" "+ l_name}"
 
   users << {
     first_name: f_name,
     last_name: l_name,
-    user_location:      ['lisboa', 'rio de janeiro', 'luanda', 'estoril', 'benfica'].sample,
+    user_location:      ['Universidade Lisboa', 'Universidade de São Paulo', 'Universidade Agustinho Neto', 'E.S.T.E.', 'SLB'].sample,
     password:           "123456",
     email:              "#{f_name + l_name}@mat3rial_unicorn.com"
   }
-  puts "user created"
+  puts "<< user added"
 }
-User.create!(users)
+puts "if a seeding fails it is due to repetition in the email (having mutiple exact same names)"
+User.create!(users.uniq!)
 puts 'Finished!'
