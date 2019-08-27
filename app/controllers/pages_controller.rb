291e0bb1ae2423d78c11a7b7e78f00da337968f1
karim ussene <karimussene@gmail.com>
2019-08-27 18:56:46 +0100

    set items#index as root page, did cards and baner for home page, cosmetic modifications to seeds file (sorry Ijoline)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  # skip_before_action :authenticate_user!, only: :home
   def home
   end
   # comment commit test
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+
+  root to: 'items#index'
+
   resources :users, except: [:show, :index, :new]
   resources :items
   resources :transactions, except: [:edit, :update, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
