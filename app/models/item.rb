9f2cba609f5a7761a329b5d50f0c1e400a1379d9
RitaLaia <ritalaiamatos@gmail.com>
2019-08-27 18:02:15 +0100

    show page update

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
-  NON_VALIDATABLE_ATTRS = ["id", "created_at", "updated_at"] # or any other attribute that does not need validation
-  VALIDATABLE_ATTRS = Item.attribute_names.reject { |attr| NON_VALIDATABLE_ATTRS.include?(attr) }
-  validates_presence_of VALIDATABLE_ATTRS
+  # NON_VALIDATABLE_ATTRS = ["id", "created_at", "updated_at"] # or any other attribute that does not need validation
+  # VALIDATABLE_ATTRS = Item.attribute_names.reject { |attr| NON_VALIDATABLE_ATTRS.include?(attr) }
+  # validates_presence_of VALIDATABLE_ATTRS
   validates :sold, default: false
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.new(first_name: "Rita", last_name: "The Queen", user_location: "Monsanto" , email: "thequeen@lw.org", password: "lookingforking")
+user.save!
+
+item1 = Item.create(title: 'pencils', description: 'Used', category: 'pencils', location:'Lisbon', sold:'not sold', user_id:'1', price: 50.0)
+item1.save!
+
