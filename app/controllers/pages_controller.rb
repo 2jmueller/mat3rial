a2f0f15dca8a644c5866af100de004e91f6a5d8a
Julius Mueller <jmueller98@me.com>
2019-08-29 16:21:54 +0100

    struggling with the filtering of stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,8 @@ class PagesController < ApplicationController
   def search
     # take a query string from the form on the root page, query our database for that string and return it on a different page
     @query = params[:query]
-    @items = Item.search_by_title_and_description_and_category(@query).geocoded
+    fetch_categories
+    @items = Item.search_by_title_and_description_and_category(@query).where(sold: 'false').geocoded
     @markers = @items.map do |item|
       {
         lat: item.latitude,
@@ -15,5 +16,11 @@ class PagesController < ApplicationController
         infoWindow: render_to_string(partial: "info_window", locals: { item: item })
       }
     end
+  end
+
+  private
+
+  def fetch_categories
+    @categories = Item.distinct.pluck(:category)
   end
 end
